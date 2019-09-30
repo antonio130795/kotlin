26770a6432f8e7f622e09511853ac37ba1bb94dc
antonio130795 <40267267+antonio130795@users.noreply.github.com>
2019-09-30 19:21:39 +0200

    Ej9

--- src/Ejercicio9.kt
+++ src/Ejercicio9.kt
@@ -36,7 +36,7 @@ fun media(numeros: IntArray): Double{
 
 fun moda(numeros: IntArray): Int{
 
-    var moda = IntArray(100){ 0}
+    var moda = IntArray(101){ 0}
     
     for (valor in numeros){
         moda[valor] += 1
@@ -48,12 +48,32 @@ fun moda(numeros: IntArray): Int{
             maximo = valor
         }
     }
-    return 1
+
+    var contador: Int = 0
+
+    for (posicion in moda.indices){
+        if(moda.get(posicion) == maximo)
+            contador = posicion
+    }
+
+    return contador
+}
+fun frecuenciaDeAparicion(numeros: IntArray){
+    var fAparicion = IntArray(101){ 0}
+
+    for (valor in numeros){
+        fAparicion[valor] += 1
+    }
+    for (posicion in fAparicion.indices){
+        println("El numero " + posicion + " aparece " + fAparicion.get(posicion) + " veces")
+    }
 }
 fun main(){
     val numeros = IntArray(2000){ Random.nextInt(1, 101)}
     println(minimo(numeros))
     println(maximo(numeros))
     println(media(numeros))
+    println(moda(numeros))
+    println(frecuenciaDeAparicion(numeros))
 }
 
